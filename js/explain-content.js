a21c4445172139a8d12fb35dccb5eb67d1025ed5
Klaus Stein <apps@istik.de>
2017-05-17 10:15:57 +0200

    Add code and examples for single-element maps with data parameters.
    This also includes some extra js to display html source code
    from the page on the page.

---
+++ js/explain-content.js
@@ -0,0 +1,49 @@
+// let's have some anonymous function (as a scope)
+(function() {
+
+		function style(str) {
+				const fstr = str.replace(/\t/g, "  ");
+				const ind = /^ +/m.exec(fstr);
+				if(!ind) { return str; }
+				
+				const ii = ind[0].length - 2;
+				const re = new RegExp("^ {" + ii + "}", "mg");
+				return fstr.replace(re, "");
+		}
+		
+		function explain(elem) {
+				var fc = elem.getElementsByTagName("figcaption")[0];
+				if(!fc) {
+						fc = document.createElement("figcaption");
+						elem.appendChild(fc);
+				}
+				const inner = elem.querySelector(".easymap");
+				
+				const code =  document.createElement("code");
+				code.setAttribute("class", "lang-html");
+				code.textContent = style(inner.outerHTML);
+				fc.appendChild(code);
+		}
+
+		
+		/* The startup sequence */
+		function explain_em(cls) {
+				const elems = document.getElementsByClassName(cls || "explain-content"); 
+				for(var i=0; i<elems.length; i++) {
+						explain(elems[i]);
+				}
+		}
+		// we create something like $(document).ready on our own
+		//to avoid JQuery dependency (and we don't support IE before 9).
+		var done = false; // only call this once!
+		function init_once() {
+				if(!done) {
+						explain_em();
+						done = true;
+				}
+		}
+		document.addEventListener("DOMContentLoaded", init_once); // register
+		if(document.readyState !== "loading") { // we may be late
+				init_once();                        // so call it
+		}
+})();
--- js/leaflet-easymap.js
+++ js/leaflet-easymap.js
@@ -3,6 +3,7 @@
 		if(!("L" in window)) {
 				throw new ReferenceError("You must load Leaflet to use easymap!");
 		}
+		// Default config
 		L.easymap = {
 				config: {
 						basemaps: {
@@ -11,19 +12,74 @@
 										attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 								}
 						},
+						default_basemap: "osm_org",
 						origin: [51.48, 0],
 						zoom:14
 				}
 		};
 
-		/* create the map */
-		function mappify(elem) {
-				const map = L.map(elem);
-				const basemap = L.easymap.config.basemaps.osm_org;
+		function data(elem, attr) {
+				return elem.getAttribute("data-" + attr);
+		}
 
-				map.setView(L.easymap.config.origin,L.easymap.config.zoom);
+		function booldata(elem, attr) {
+				return ((data(elem, attr)||"").toUpperCase() == "TRUE");
+		}
+		
+		
+		/* 
+		 * determine the origin from data attributes
+		 */
+		function getOrigin(elem, mpos) {
+				const d = data(elem, "origin");
+				const o = (d && d.split(",")) ||             // origin given as attribute
+									(mpos && mpos.slice(0)) ||         // or as marker position
+									L.easymap.config.origin.slice(0);  // or the default
+				const lat = data(elem, "lat");               // directly given lat and lon
+				const lon = data(elem, "lon");               // have highest priority
+				if(lat) { o[0] = lat; }
+				if(lon) { o[1] = lon; }
+				return o;
+		}
+		
+		/* 
+		 * create the map on elem
+		 */
+		function mappify(elem) {
+				// read the data attributes
+				const basemap = L.easymap.config.basemaps.osm_org;
+				const markertxt = data(elem, "marker");
+				const popuptxt = data(elem, "popup");
+				const mpos = markertxt && markertxt.split(",");
+				const locate = booldata(elem, "locate");
+				const origin = getOrigin(elem, mpos);
+				
+				const map = L.map(elem); // create the map
+				map.setView(origin, // set the view
+										data(elem, "zoom") || L.easymap.config.zoom);
+
+				if(locate) {
+						map.locate({ setView: true });
+				}
+
+				// Todo: improve baselayer selection (more baselayers)
 				L.tileLayer(basemap.url,
 										{attribution: basemap.attribution}).addTo(map);
+
+
+				const marker = mpos && L.marker(mpos).addTo(map);
+
+				// a popup
+				const popup = popuptxt &&
+							        L.popup({closeOnClick: false}).setContent(popuptxt);
+				if(popup) {
+						if (marker) {
+								marker.bindPopup(popup).openPopup();
+						} else {
+								popup.setLatLng(origin).openOn(map);
+						}
+				}
+				
 		}
 
 
