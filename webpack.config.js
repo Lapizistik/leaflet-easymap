3206f517aa5c0dfa3cd2df500e44f8f09ef8eef5
Klaus Stein <apps@istik.de>
2019-09-17 11:39:12 +0200

    Add source maps for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,39 @@ const path = require('path');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  // Tells Webpack which built-in optimizations to use
-  // In 'production' mode, Webpack will minify and uglify our JS code
-  // If you leave this out, Webpack will default to 'production'
-  mode: devMode ? 'development' : 'production',
-
-  // Webpack needs to know where to start the bundling process,
-  // so we define the main JS and Sass files, both under
-  // the './src' directory
+		// Tells Webpack which built-in optimizations to use
+		// In 'production' mode, Webpack will minify and uglify our JS code
+		// If you leave this out, Webpack will default to 'production'
+		mode: devMode ? 'development' : 'production',
+		devtool: 'inline-source-map',
+		// Webpack needs to know where to start the bundling process,
+		// so we define the main JS and Sass files, both under
+		// the './src' directory
 		entry: {
 				'leaflet-easymap': './src/leaflet-easymap.js',
 				'explain-content': './src/explain-content.js'
 		},
-
-  // This is where we define the path where Webpack will place
-  // the bundled JS file
-  output: {
-			// The name of the output bundle. Path is also relative
-    // to the output path
-			filename: '[name].js',
-			path: __dirname + '/js'
-  },
-  module: {
-    // Array of rules that tells Webpack how the modules (output)
-    // will be created
-    rules: [
-      {
-        // Look for JavaScript files and apply the babel-loader
-        // excluding the './node_modules' directory. It uses the
-        // configuration in `.babelrc`
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-    ]
-  }
+		
+		// This is where we define the path where Webpack will place
+		// the bundled JS file
+		output: {
+				// The name of the output bundle. Path is also relative
+				// to the output path
+				filename: '[name].js',
+				path: __dirname + '/js'
+		},
+		module: {
+				// Array of rules that tells Webpack how the modules (output)
+				// will be created
+				rules: [
+						{
+								// Look for JavaScript files and apply the babel-loader
+								// excluding the './node_modules' directory. It uses the
+								// configuration in `.babelrc`
+								test: /\.(js)$/,
+								exclude: /node_modules/,
+								use: ['babel-loader']
+						},
+				]
+		}
 };
